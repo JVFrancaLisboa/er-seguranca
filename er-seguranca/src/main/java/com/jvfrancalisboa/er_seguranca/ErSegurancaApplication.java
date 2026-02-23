4f7e058fb1100a5ebb84689dbbdbd18f7d102f8b
JVFrancaLisboa <logiccoder88@gmail.com>
2026-02-23 19:58:46 -0300

    feat: added spring framework

---
+++ er-seguranca/src/main/java/com/jvfrancalisboa/er_seguranca/ErSegurancaApplication.java
@@ -0,0 +1,13 @@
+package com.jvfrancalisboa.er_seguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErSegurancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErSegurancaApplication.class, args);
+	}
+
+}
---
+++ er-seguranca/src/test/java/com/jvfrancalisboa/er_seguranca/ErSegurancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvfrancalisboa.er_seguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErSegurancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
